54c1e447bc2e2611120eca950a61102ad5740184
BlasAST <blas.siguencia1@gmail.com>
2024-11-09 14:42:12 +0100

    Simple, recopilación datos de los formularios

--- CitasMedicas/app.py
+++ CitasMedicas/app.py
@@ -1,27 +1,38 @@
-from flask import Flask, render_template
-# from flask_session import Session
+from flask import Flask, render_template, request
+from flask_session import Session
 app = Flask(__name__,template_folder="templates",static_url_path="/static")
-# Session(app)
-# app.config['SECRET_KEY'] = 'supersecreto'
-# app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+app.config['SECRET_KEY'] = 'supersecreto'
+app.config['SESSION_TYPE'] = 'filesystem'
 @app.route('/')
 def index():
     return render_template("index/index.html")
-
+# * Ruta para la redirección a iniciar o registrarse
 @app.route("/logReg")
 def logReg():
     return render_template("index/logReg.html")
-
-@app.route("/inicioSesion")
+# * Ruta inicio Sesión y recopilación de los datos para comprobarlo en la base de datos
+@app.route("/inicioSesion", methods=['GET'] )
 def inicioSesion():
+    usuario = request.args.get('usuario')
+    contrasenia = request.args.get('contrasenia')
     return render_template("index/login.html")
-
-@app.route("/registrarse")
+# * Ruta registrarse y recopilación de datos para registrarlos en la base de datos
+@app.route("/registrarse", methods=['GET'])
 def registrarse():
-    return render_template("index/register.html")
+    nombre = request.args.get('nombre')
+    apellidos = request.args.get('apellidos')
+    dni = request.args.get('dni')
+    fechaNacimiento = request.args.get('fechaNacimiento')
+    usuario = request.args.get('usuario')
+    contrasenia = request.args.get('contrasenia')
+    contrasenia2 = request.args.get('contrasenia2')
 
-@app.route("/lostpass")
+    return render_template("index/register.html")
+# * Ruta recuperar contraseña y recopilación de datos para enviar correo de recuperación
+@app.route("/lostpass", methods=['GET'])
 def lostPass():
+    recuperar = request.args.get('recuperar')
     return render_template("index/lostPassword.html")
 if __name__ == '__main__':
     app.run(debug=True)
